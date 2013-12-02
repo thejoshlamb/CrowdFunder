68ae3367ebd921da8cba5cd94923ae09a286ad82
joffet <joffet@gmail.com>
2013-12-02 13:57:43 -0500

    Rails new started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrowdFunder::Application.config.secret_key_base = 'd99fcf6e36ceeea59f035c11b1a445fa0930ce9e6f318ec7f88b83b16c3f60daf322eddefe9435ece3a91e1c9731d50f1dde58c2d4c06027457cd7ba44a46016'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CrowdFunder::Application.config.session_store :cookie_store, key: '_CrowdFunder_session'
