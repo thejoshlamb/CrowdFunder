a5611dca226a18144e3c2f2a8e440f5ae47c3b65
thejoshlamb <joshlamb@gmail.com>
2013-12-02 19:13:54 -0500

    added basic actions to comments model

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,2 +1,56 @@
 class PledgesController < ApplicationController
+
+	def index
+	  @projects = Pledge.all
+
+	  respond_to do |format|
+	    format.js
+	    format.html
+	  end
+	end
+
+	def show
+	  @pledge = Pledge.find(params[:id])
+	  #@comment = Comment.new(:pledge_id => @pledge.id)
+	end
+
+	def new
+	  @pledge = Pledge.new
+	end
+
+	def create
+	  @pledge = Pledge.new(pledge_params)
+	  
+	  if @pledge.save
+	    redirect_to pledges_url
+	  else
+	    render :new
+	  end
+	end
+
+	def edit
+	  @pledge = Pledge.find(params[:id])
+	end
+
+	def update
+	  @pledge = Pledge.find(params[:id])
+
+	  if @pledge.update_attributes(pledge_params)
+	    redirect_to pledges_url
+	  else
+	    render :edit
+	  end
+	end
+
+	def destroy
+	  @pledge = pledge.find(params[:id])
+	  @pledge.destroy
+	  redirect_to pledges_url
+	end
+
+	private
+	def pledge_params
+	  params.require(:pledge).permit(:amount, :user_id)
+	end
+
 end
