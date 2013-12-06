1757e3dbb4e4b7e52ae6f52283b05c2a28386fa2
thejoshlamb <joshlamb@gmail.com>
2013-12-06 17:19:50 -0500

    fixed gravatar method to work with users who have no email, ie twitter logins

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,13 @@
 module UsersHelper
 
 	def gravatar_for(user,size=200)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.username, class: "gravatar")
-  end
+		if user.email
+	    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+	    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+	    image_tag(gravatar_url, alt: user.username, class: "gravatar")
+	  else
+	  	image_tag "http://www.gravatar.com/avatar/0?s=#{size}"
+  	end
 
+	end
 end
